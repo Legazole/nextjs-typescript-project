010a784880c5439a3931907c32743922edcfb652
Legazole <LegazoleNFT@gmail.com>
2023-03-31 19:03:00 +0200

    started project

---
+++ nextjs-intro-project/pages/data.tsx
@@ -0,0 +1,15 @@
+function DataPage(){
+
+
+}
+
+/*
+export const getStaticProps = async (context) => {
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const {result} = await res.json();
+
+    return
+}
+*/
+
+export default DataPage;
